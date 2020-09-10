09a27fb2ce205d5c14b353b920a7213be5ea0009
sampuka <matddi@gmail.com>
2020-09-10 11:51:12 +0200

    Initial

---
+++ plugin/src/EITPlugin.cpp
@@ -0,0 +1,292 @@
+#include "EITPlugin.hpp"
+
+EITPlugin::EITPlugin():
+    RobWorkStudioPlugin("EITPluginUI", QIcon(":/pa_icon.png"))
+{
+    setupUi(this);
+
+    // Connect UI components to member functions
+    connect(ui_home_button, SIGNAL(pressed()), this, SLOT(home_button()));
+    //connect(_btn_im    ,SIGNAL(pressed()), this, SLOT(btnPressed()) );
+
+    _framegrabber = NULL;
+
+    _cameras = {"Camera_Right", "Camera_Left"};
+    _cameras25D = {"Scanner25D"};
+}
+
+EITPlugin::~EITPlugin()
+{
+    delete _textureRender;
+    delete _bgRender;
+}
+
+void EITPlugin::initialize()
+{
+    log().info() << "INITALIZE" << "\n";
+
+    getRobWorkStudio()->stateChangedEvent().add(std::bind(&EITPlugin::stateChangedListener, this, std::placeholders::_1), this);
+
+    // Get path to project from environment
+    char* projectpath = std::getenv("EITDIR");
+
+    if (projectpath == NULL)
+    {
+        std::cerr << "EITDIR environment variable not set! Perform \"export EITDIR=/home/user/eit_project/\", with the correct path." << std::endl;
+    }
+    else
+    {
+        rw::models::WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(std::string(projectpath) + "/workcell/Scene.wc.xml");
+
+        if (wc == nullptr)
+        {
+            std::cerr << "Unable to autoload workcell! Maybe EITDIR environment variable not set correctly?" << std::endl;
+        }
+        else
+        {
+            getRobWorkStudio()->setWorkCell(wc);
+        }
+    }
+}
+
+void EITPlugin::open(rw::models::WorkCell* workcell)
+{
+    log().info() << "OPEN" << "\n";
+    rws_wc = workcell;
+    rws_state = rws_wc->getDefaultState();
+
+    log().info() << workcell->getFilename() << "\n";
+
+    if (rws_wc != NULL)
+    {
+        // Add the texture render to this workcell if there is a frame for texture
+        rw::kinematics::Frame* textureFrame = rws_wc->findFrame("MarkerTexture");
+        if (textureFrame != NULL)
+        {
+            getRobWorkStudio()->getWorkCellScene()->addRender("TextureImage",_textureRender,textureFrame);
+        }
+
+        // Add the background render to this workcell if there is a frame for texture
+        rw::kinematics::Frame* bgFrame = rws_wc->findFrame("Background");
+        if (bgFrame != NULL)
+        {
+            getRobWorkStudio()->getWorkCellScene()->addRender("BackgroundImage",_bgRender,bgFrame);
+        }
+
+        // Create a GLFrameGrabber if there is a camera frame with a Camera property set
+        rw::kinematics::Frame* cameraFrame = rws_wc->findFrame(_cameras[0]);
+        if (cameraFrame != NULL)
+        {
+            if (cameraFrame->getPropertyMap().has("Camera"))
+            {
+                // Read the dimensions and field of view
+                double fovy;
+                int width,height;
+                std::string camParam = cameraFrame->getPropertyMap().get<std::string>("Camera");
+                std::istringstream iss (camParam, std::istringstream::in);
+                iss >> fovy >> width >> height;
+                // Create a frame grabber
+                _framegrabber = new rwlibs::simulation::GLFrameGrabber(width,height,fovy);
+                rw::graphics::SceneViewer::Ptr gldrawer = getRobWorkStudio()->getView()->getSceneViewer();
+                _framegrabber->init(gldrawer);
+            }
+        }
+
+        rw::kinematics::Frame* cameraFrame25D = rws_wc->findFrame(_cameras25D[0]);
+        if (cameraFrame25D != NULL)
+        {
+            if (cameraFrame25D->getPropertyMap().has("Scanner25D"))
+            {
+                // Read the dimensions and field of view
+                double fovy;
+                int width,height;
+                std::string camParam = cameraFrame25D->getPropertyMap().get<std::string>("Scanner25D");
+                std::istringstream iss (camParam, std::istringstream::in);
+                iss >> fovy >> width >> height;
+                // Create a frame grabber
+                _framegrabber25D = new rwlibs::simulation::GLFrameGrabber25D(width,height,fovy);
+                rw::graphics::SceneViewer::Ptr gldrawer = getRobWorkStudio()->getView()->getSceneViewer();
+                _framegrabber25D->init(gldrawer);
+            }
+        }
+
+        UR_left = rws_wc->findDevice("UR-6-85-5-A_Left");
+        UR_right = rws_wc->findDevice("UR-6-85-5-A_Right");
+    }
+}
+
+
+void EITPlugin::close()
+{
+    log().info() << "CLOSE" << "\n";
+
+    // Remove the texture render
+    rw::kinematics::Frame* textureFrame = rws_wc->findFrame("MarkerTexture");
+    if (textureFrame != NULL)
+    {
+        getRobWorkStudio()->getWorkCellScene()->removeDrawable("TextureImage",textureFrame);
+    }
+
+    // Remove the background render
+    rw::kinematics::Frame* bgFrame = rws_wc->findFrame("Background");
+    if (bgFrame != NULL)
+    {
+        getRobWorkStudio()->getWorkCellScene()->removeDrawable("BackgroundImage",bgFrame);
+    }
+
+    // Delete the old framegrabber
+    if (_framegrabber != NULL)
+    {
+        delete _framegrabber;
+    }
+
+    _framegrabber = NULL;
+    rws_wc = NULL;
+}
+
+cv::Mat EITPlugin::toOpenCVImage(const rw::sensor::Image& img)
+{
+    cv::Mat res(img.getHeight(),img.getWidth(), CV_8SC3);
+    res.data = (uchar*)img.getImageData();
+    return res;
+}
+
+void EITPlugin::get25DImage()
+{
+    if (_framegrabber25D != NULL)
+    {
+        for( int i = 0; i < _cameras25D.size(); i++)
+        {
+            // Get the image as a RW image
+            rw::kinematics::Frame* cameraFrame25D = rws_wc->findFrame(_cameras25D[i]); // "Camera");
+            _framegrabber25D->grab(cameraFrame25D, rws_state);
+
+            //const Image& image = _framegrabber->getImage();
+
+            const rw::geometry::PointCloud* img = &(_framegrabber25D->getImage());
+
+            std::ofstream output(_cameras25D[i] + ".pcd");
+            output << "# .PCD v.5 - Point Cloud Data file format\n";
+            output << "FIELDS x y z\n";
+            output << "SIZE 4 4 4\n";
+            output << "TYPE F F F\n";
+            output << "WIDTH " << img->getWidth() << "\n";
+            output << "HEIGHT " << img->getHeight() << "\n";
+            output << "POINTS " << img->getData().size() << "\n";
+            output << "DATA ascii\n";
+            for(const auto &p_tmp : img->getData())
+            {
+                rw::math::Vector3D<float> p = p_tmp;
+                output << p(0) << " " << p(1) << " " << p(2) << "\n";
+            }
+            output.close();
+        }
+    }
+}
+
+void EITPlugin::getImage()
+{
+    if (_framegrabber != NULL)
+    {
+        for( int i = 0; i < _cameras.size(); i++)
+        {
+            // Get the image as a RW image
+            rw::kinematics::Frame* cameraFrame = rws_wc->findFrame(_cameras[i]); // "Camera");
+            _framegrabber->grab(cameraFrame, rws_state);
+
+            const rw::sensor::Image* rw_image = &(_framegrabber->getImage());
+
+            // Convert to OpenCV matrix.
+            cv::Mat image = cv::Mat(rw_image->getHeight(), rw_image->getWidth(), CV_8UC3, (rw::sensor::Image*)rw_image->getImageData());
+
+            // Convert to OpenCV image
+            cv::Mat imflip, imflip_mat;
+            cv::flip(image, imflip, 1);
+            cv::cvtColor( imflip, imflip_mat, cv::COLOR_RGB2BGR);
+
+            cv::imwrite(_cameras[i] + ".png", imflip_mat );
+
+            // Show in QLabel
+            QImage img(imflip.data, imflip.cols, imflip.rows, imflip.step, QImage::Format_RGB888);
+            QPixmap p = QPixmap::fromImage(img);
+            unsigned int maxW = 480;
+            unsigned int maxH = 640;
+            //_label->setPixmap(p.scaled(maxW,maxH,Qt::KeepAspectRatio));
+        }
+    }
+}
+
+void EITPlugin::stateChangedListener(const rw::kinematics::State& state)
+{
+    rws_state = state;
+}
+
+void EITPlugin::home_button()
+{
+    std::cout << "Home button pressed!" << std::endl;
+}
+
+bool EITPlugin::checkCollisions(rw::models::Device::Ptr device, const rw::kinematics::State &state, const rw::proximity::CollisionDetector &detector, const rw::math::Q &q)
+{
+    rw::kinematics::State testState;
+    rw::proximity::CollisionDetector::QueryResult data;
+    bool colFrom;
+
+    testState = state;
+    device->setQ(q,testState);
+    colFrom = detector.inCollision(testState,&data);
+    if (colFrom)
+    {
+        std::cout << "Configuration in collision: " << q << std::endl;
+        std::cout << "Colliding frames: " << std::endl;
+        rw::kinematics::FramePairSet fps = data.collidingFrames;
+        for (rw::kinematics::FramePairSet::iterator it = fps.begin(); it != fps.end(); it++)
+        {
+            std::cout << (*it).first->getName() << " " << (*it).second->getName() << std::endl;
+        }
+        return false;
+    }
+    return true;
+}
+
+void EITPlugin::createPathRRTConnect(rw::math::Q from, rw::math::Q to, double extend, double maxTime)
+{
+    /*
+    _device->setQ(from,_state);
+    getRobWorkStudio()->setState(_state);
+    CollisionDetector detector(_wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
+    PlannerConstraint constraint = PlannerConstraint::make(&detector,_device,_state);
+    QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(_device),constraint.getQConstraintPtr());
+    QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
+    QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
+
+    _path.clear();
+    if (!checkCollisions(_device, _state, detector, from))
+        cout << from << " is in colission!" << endl;
+    if (!checkCollisions(_device, _state, detector, to))
+        cout << to << " is in colission!" << endl;
+    Timer t;
+    t.resetAndResume();
+    planner->query(from,to,_path,maxTime);
+    t.pause();
+
+    if (t.getTime() >= maxTime)
+    {
+        cout << "Notice: max time of " << maxTime << " seconds reached." << endl;
+    }
+
+    const int duration = 10;
+
+    if(_path.size() == 2)
+    {   // The interpolated path between Q start and Q goal is collision free. Set the duration with respect to the desired velocity
+        LinearInterpolator<Q> linInt(from, to, duration);
+        QPath tempQ;
+        for(int i = 0; i < duration+1; i++)
+        {
+            tempQ.push_back(linInt.x(i));
+        }
+
+        _path=tempQ;
+    }
+    */
+}
---
+++ plugin/src/EITPlugin.hpp
@@ -0,0 +1,71 @@
+#ifndef EITPLUGIN_HPP
+#define EITPLUGIN_HPP
+
+// RobWork includes
+#include <rw/rw.hpp>
+#include <rw/kinematics/State.hpp>
+#include <rw/loaders/ImageLoader.hpp>
+#include <rw/loaders/WorkCellFactory.hpp>
+#include <rw/models/WorkCell.hpp>
+#include <rw/trajectory/LinearInterpolator.hpp>
+
+#include <rwlibs/opengl/RenderImage.hpp>
+#include <rwlibs/simulation/GLFrameGrabber.hpp>
+#include <rwlibs/simulation/GLFrameGrabber25D.hpp>
+#include <rwlibs/pathplanners/rrt/RRTPlanner.hpp>
+#include <rwlibs/pathplanners/rrt/RRTQToQPlanner.hpp>
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+
+// RobWorkStudio includes
+#include <rws/RobWorkStudio.hpp>
+#include <rws/RobWorkStudioPlugin.hpp>
+#include <RobWorkStudioConfig.hpp> // For RWS_USE_QT5 definition
+
+// OpenCV 3
+#include <opencv2/opencv.hpp>
+
+// Qt
+#include "ui_EITPlugin.h"
+
+class EITPlugin: public rws::RobWorkStudioPlugin, private Ui::EITPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES( rws::RobWorkStudioPlugin )
+    Q_PLUGIN_METADATA(IID "dk.sdu.mip.Robwork.RobWorkStudioPlugin/0.1" FILE "plugin.json")
+
+    public:
+        EITPlugin();
+        virtual ~EITPlugin();
+
+        virtual void open(rw::models::WorkCell* workcell);
+        virtual void close();
+        virtual void initialize();
+
+    private slots:
+        void getImage();
+        void get25DImage();
+
+        void stateChangedListener(const rw::kinematics::State& state);
+
+        void home_button();
+
+        bool checkCollisions(rw::models::Device::Ptr device, const rw::kinematics::State &state, const rw::proximity::CollisionDetector &detector, const rw::math::Q &q);
+        void createPathRRTConnect(rw::math::Q from, rw::math::Q to, double extend, double maxTime);
+
+    private:
+        static cv::Mat toOpenCVImage(const rw::sensor::Image& img);
+
+        rw::models::WorkCell::Ptr rws_wc;
+        rw::kinematics::State rws_state;
+        rw::models::Device::Ptr UR_left;
+        rw::models::Device::Ptr UR_right;
+
+        // Misc
+        rwlibs::opengl::RenderImage *_textureRender, *_bgRender;
+        rwlibs::simulation::GLFrameGrabber* _framegrabber;
+        rwlibs::simulation::GLFrameGrabber25D* _framegrabber25D;
+        std::vector<std::string> _cameras;
+        std::vector<std::string> _cameras25D;
+};
+
+#endif /*EITPLUGIN_HPP*/
