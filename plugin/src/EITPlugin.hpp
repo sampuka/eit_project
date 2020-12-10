4da687127b94048faedb337f5d4cc525973a9608
sampuka <matddi@gmail.com>
2020-12-10 11:30:34 +0100

    Increased place positions to 5

--- plugin/src/EITPlugin.hpp
+++ plugin/src/EITPlugin.hpp
@@ -103,7 +103,7 @@ class EITPlugin: public rws::RobWorkStudioPlugin, private Ui::EITPlugin
         rw::math::Q home_Q;
         rw::math::Q pick_approach_Q;
         rw::math::Q pick_Q;
-        const unsigned int place_position_count = 3; // Must be at least 2 for now
+        const unsigned int place_position_count = 5; // Must be at least 2 for now
         const double x_lim1 = 0.35;
         const double x_lim2 = -0.35;
         std::vector<rw::math::Q> place_approach_Qs;
